962c8512ff355a9191b8f081a6390bba8731b4fa
龔泓愷 <40812693+Bourbon0212@users.noreply.github.com>
2019-01-11 12:54:19 +0800

    0110

--- questionnaire/get_res_db.py
+++ questionnaire/get_res_db.py
@@ -1,10 +1,14 @@
-
 import sqlite3
 
-def get_feedback(feedback, userid, quick):
+from assessment.get_latest_assessment_id_db import get_latest_assessment_id_db
+# 把feedback寫進db
 
+def get_feedback(feedback, userid):
+# def get_feedback(feedback, userid, location):
     #DB開關
-    conn = sqlite3.connect('response.db')
+    conn = sqlite3.connect('response.db') 
+    # conn = psycopg2.connect("dbname=pmdianapg user=postgres host=hci.dianalab.net port=10710 password=" + os.environ.get("DBPASSWORD"))
+
     c = conn.cursor()
 
     #建response表格
@@ -17,16 +21,18 @@ def get_feedback(feedback, userid, quick):
             assessment_id integer default 0,
             building_id integer default 0);""")
 
-
-
-    a = {i:None for i in range(65, 78)} if quick else {i:None for i in range(1, 65)}
+    # a = {i:None for i in range(65, 78)} if quick else {i:None for i in range(1, 65)}
+    a = {i:None for i in range(1, 78)}
 
     for i,value in feedback:
         a[i] = value
 
+
+    assessment_id = get_latest_assessment_id_db()[0]
+
     for i, value in a.items():
         yn = 1 if value is None else 0
-        c.execute('INSERT INTO responses (YN, description, userid, question_id) VALUES (?,?,?,?);', (yn, value, userid, i))
+        c.execute('INSERT INTO responses (YN, description, userid, question_id, assessment_id) VALUES (?,?,?,?,?);', (yn, value, userid, i, assessment_id))
 
     conn.commit()
     conn.close()
